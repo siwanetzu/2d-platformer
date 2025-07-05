40ac3924719fc0fb52e904ec641a3c33dc453564
siwanetzu <domgolab@gmail.com>
2025-07-05 19:05:58 +0100

    Animation player referenced in script to play fly animation

--- enemy.gd
+++ enemy.gd
@@ -7,6 +7,10 @@ extends Area2D
 @onready var start_pos : Vector2 = global_position
 @onready var target_pos : Vector2 = global_position + move_direction
 
+
+func _ready() -> void:
+	$AnimationPlayer.play("fly")
+
 func _physics_process(delta: float) -> void:
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
 	if global_position == target_pos:
