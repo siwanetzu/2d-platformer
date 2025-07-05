4521be8179ee809d5e5a45c4998a7a104089d340
siwanetzu <domgolab@gmail.com>
2025-07-05 19:32:58 +0100

    on body entered for the coin + script to increase score

--- coin.gd
+++ coin.gd
@@ -16,3 +16,12 @@ func _physics_process(delta: float) -> void:
 	# bob up and down
 	var y_pos = ((1 + sin(time * bob_speed)) /2) * bob_height
 	global_position.y = start_pos.y - y_pos
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("Player"):
+		return
+		
+	body.increase_score(1)
+	queue_free()
--- player.gd
+++ player.gd
@@ -68,3 +68,6 @@ func take_damage (amount: int):
 
 func game_over ():
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+
+func increase_score (amount : int):
+	pass
