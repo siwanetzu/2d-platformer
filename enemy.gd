c22af76e7fcc7e65697e85f3695faf9ad5001b57
siwanetzu <domgolab@gmail.com>
2025-07-05 18:35:57 +0100

    Enemy implementation + script (physics process) and movement

---
+++ enemy.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var move_direction : Vector2 
+@export var move_speed : float = 20
+
+@onready var start_pos : Vector2 = global_position
+@onready var target_pos : Vector2 = global_position + move_direction
+
+func _physics_process(delta: float) -> void:
+	global_position = global_position.move_toward(target_pos, move_speed * delta)
+	if global_position == target_pos:
+		if target_pos == start_pos:
+			target_pos = start_pos + move_direction
+		else:
+			target_pos = start_pos
